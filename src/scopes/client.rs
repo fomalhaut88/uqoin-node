354223c40555a3c3639c8b37ebf8fa97a77928be
Alexander X <alexfomalhaut@gmail.com>
2025-05-01 06:32:31 +0300

    Coins request optimization.

--- src/scopes/client.rs
+++ src/scopes/client.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use serde::Deserialize;
 use actix_web::{web, HttpResponse, Scope};
 use actix_web::http::header::ContentType;
@@ -9,21 +11,89 @@ use crate::utils::*;
 
 
 #[derive(Deserialize)]
-struct Query {
+struct CoinsQuery {
     wallet: String,
+    order: Option<u64>,
 }
 
 
-/// Get coins belonging to the wallet at the current last block.
+/// Get coins belonging to the wallet at the last block. If `order` is  
+/// specified, the result coins are represented as a list, else the full mapping
+/// order - coin list will be returned. It is recommended not to use the 
+/// endpoint often without `order` due to too big size of the coins map.
 async fn coins_view(appdata: WebAppData, 
-                    query: web::Query<Query>) -> APIResult {
+                    query: web::Query<CoinsQuery>) -> APIResult {
+    // Check syncing
     api_check!(!*appdata.is_syncing.read().await, "Syncing");
+
+    // Prepare wallet number
     let wallet = U256::from_hex(&query.wallet);
+
+    // Get state that contains the coins information
     let state = appdata.state.read().await;
-    if let Some(coins_map) = state.get_coins(&wallet) {
-        Ok(HttpResponse::Ok().json(coins_map))
+
+    // If order is specified in the query, else return full map
+    if let Some(order) = query.order {
+        // If coins map is found in the state, else return empty empty list
+        if let Some(coins_map) = state.get_coins(&wallet) {
+            // If coins is found for the order, else return empty list
+            if let Some(coins) = coins_map.get(&order) {
+                // Return coins as list
+                Ok(HttpResponse::Ok().json(coins))
+            } else {
+                // Return empty list
+                Ok(HttpResponse::Ok().insert_header(ContentType::json())
+                                     .body("[]"))
+            }
+        } else {
+            // Return empty list
+            Ok(HttpResponse::Ok().insert_header(ContentType::json())
+                                 .body("[]"))
+        }
     } else {
-        Ok(HttpResponse::Ok().insert_header(ContentType::json()).body("{}"))
+        // If coins map is found in the state, else return empty map {}
+        if let Some(coins_map) = state.get_coins(&wallet) {
+            // Return full map
+            Ok(HttpResponse::Ok().json(coins_map))
+        } else {
+            // Return empty map
+            Ok(HttpResponse::Ok().insert_header(ContentType::json()).body("{}"))
+        }
+    }
+}
+
+
+/// Same as `coins_view` but returning XOR hash instead of full coin lists.
+async fn coins_hash_view(appdata: WebAppData, 
+                         query: web::Query<CoinsQuery>) -> APIResult {
+    // Check syncing
+    api_check!(!*appdata.is_syncing.read().await, "Syncing");
+
+    // Prepare wallet number
+    let wallet = U256::from_hex(&query.wallet);
+
+    // Get state that contains the coins information
+    let state = appdata.state.read().await;
+
+    // If order is specified in the query, else return full map
+    if let Some(order) = query.order {
+        let xor = state.calc_coins_hash(&wallet, order)
+                       .unwrap_or(U256::from(0));
+        Ok(HttpResponse::Ok().json(xor))
+    } else {
+        // If coins map is found in the state, else return empty map {}
+        if let Some(coins_map) = state.get_coins(&wallet) {
+            // Return full map
+            let xor_map: HashMap<u64, U256> = coins_map.keys()
+            .map(|order| (
+                *order,
+                state.calc_coins_hash(&wallet, *order).unwrap()
+            )).collect();
+        Ok(HttpResponse::Ok().json(xor_map))
+        } else {
+            // Return empty map
+            Ok(HttpResponse::Ok().insert_header(ContentType::json()).body("{}"))
+        }
     }
 }
 
@@ -68,5 +138,6 @@ async fn send_view(appdata: WebAppData,
 pub fn load_scope() -> Scope {
     web::scope("/client")
         .route("/coins", web::get().to(coins_view))
+        .route("/coins/hash", web::get().to(coins_hash_view))
         .route("/send", web::post().to(send_view))
 }
